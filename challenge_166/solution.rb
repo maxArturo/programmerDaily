abe1221ea30aa713e4cd8ef39cd380fbac2753ca
Max Arturo Alcala Sainz <maxarturo@gmail.com>
2015-02-05 12:22:52 -0600

    fix line endings

--- challenge_166/solution.rb
+++ challenge_166/solution.rb
@@ -6,32 +6,33 @@ class HilbertDrawer
 	def initialize(degree = 1)
 		@degree = degree
 		@resFactor = 30
-		@hilbertMap = 
+		@hilbertMap =
 		{'a' =>
 			{
-				[0, 0] => [0, 'd'], 
-				[0, 1] => [1, 'a'], 
+				[0, 0] => [0, 'd'],
+				[0, 1] => [1, 'a'],
 				[1, 0] => [3, 'b'], 
 				[1, 1] => [2, 'a']
 			},
 		'b' =>
 			{
-				[0, 0] => [2, 'b'], 
-				[0, 1] => [1, 'b'], 
-				[1, 0] => [3, 'a'], 
+				[0, 0] => [2, 'b'],
+				[0, 1] => [1, 'b'],
+				[1, 0] => [3, 'a'],
 				[1, 1] => [0, 'c']
-			}, 
+			},
 		'c' =>
 			{
-				[0, 0] => [2, 'c'], 
-				[0, 1] => [3, 'd'], 
-				[1, 0] => [1, 'c'], 
+				[0, 0] => [2, 'c'],
+				[0, 1] => [3, 'd'],
+				[1, 0] => [1, 'c'],
 				[1, 1] => [0, 'b']
-			}, 
+			},
 		'd' =>
 			{
-				[0, 0] => [0, 'a'], 
-				[0, 1] => [3, 'c'], [1, 0] => [1, 'd'], 
+				[0, 0] => [0, 'a'],
+				[0, 1] => [3, 'c'],
+				[1, 0] => [1, 'd'],
 				[1, 1] => [2, 'd']
 			}
 		}
@@ -41,14 +42,14 @@ class HilbertDrawer
 		#start in the 'base' layout
 		currSquare = 'a'
 		position = 0
-				
+
 
 		(@degree - 1).step(0, -1).each{|i|
 			position <<= 2 #shift right two bits to allow bitwise ORing upcoming
 							#bits. Perfect since 0 << 2 is harmless on the first iteration
 
 			#get most-significant bit from x and y, using |i| to track what
-			#bit to look at via bitwise shifting and then getting the ANDed leftmost bit. 
+			#bit to look at via bitwise shifting and then getting the ANDed leftmost bit.
 			#Each subsequent bit corresponds to a degree, from degree - 1 to 0
 			xQuadrant = (x & (1 << i)).to_s(2)[0].to_i == 1 ? 1 : 0
 			yQuadrant = (y & (1 << i)).to_s(2)[0].to_i == 1 ? 1 : 0
@@ -82,7 +83,7 @@ class HilbertDrawer
 		return <<-SVG
 			\n<svg xmlns="http://www.w3.org/2000/svg" height="#{2 ** @degree * @resFactor}" width="#{2 ** @degree * @resFactor}">
 			\n#{self.generateSortedPath.each_cons(2).map{|x, y| "<line x1='#{x[0] * @resFactor}' y1='#{x[1] * @resFactor}' x2='#{y[0] * @resFactor}' y2='#{y[1] * @resFactor}' style=stroke:rgb(0,0,255);stroke-width:'20' />"}.join("\n")}
-			\n</svg>	
+			\n</svg>
 		SVG
 	end
 end
@@ -90,4 +91,3 @@ end
 testHilbert = HilbertDrawer.new(5)
 testHilbertOutput = testHilbert.generateSortedPath()
 puts testHilbert.outputSVG
-
